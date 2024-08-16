ec7fd4b34cfdc3ec4f73a0bb74267bb4d82548b9
Kathan0 <kathanpatel170@gmail.com>
2024-08-16 16:12:02 +0530

    Pipeline commit

--- HelloWorldTest.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    static void method(){
-       System.out.println("Hello from Java Method");
-     }   
-    public static void main(String[] args) {
-        System.out.println("Hello Java World");
-         method();
-    }
-}
